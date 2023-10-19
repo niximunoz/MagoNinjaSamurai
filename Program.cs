5d71c08a7c293d4141b83d8bbb4dc9da1a27f73c
niximunoz <niximunozr@gmail.com>
2023-10-19 12:40:40 -0300

    MagoNinjaSamurai

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Mago mago1 = new Mago("Mago1");
+Ninja ninja1 = new Ninja("Ninja1");
+Samurai samurai1 = new Samurai("Samurai1");
+
+mago1.ShowInfo();
+Console.WriteLine("--------------------------");
+ninja1.ShowInfo();
+Console.WriteLine("--------------------------");
+samurai1.ShowInfo();
+Console.WriteLine("--------------------------");
+
+mago1.Attack(samurai1);
+mago1.ShowInfo();
+Console.WriteLine("--------------------------");
+samurai1.ShowInfo();
+Console.WriteLine("--------------------------");
